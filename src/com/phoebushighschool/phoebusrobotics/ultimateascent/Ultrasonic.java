8b26193ec2825cb83a128f3af79602735b37bc16
DJ <DJ@192.168.1.109>
2013-01-19 17:58:34 -0500

    last change

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.phoebushighschool.phoebusrobotics.ultimateascent;
+package com.PhoebusHighSchool.PhoebusRobotics.UltimateAscent;
 
 import edu.wpi.first.wpilibj.AnalogModule;
 
