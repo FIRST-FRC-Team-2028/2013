38d349dc46d526618ad9eb846c57021e61a34da7
DJ <DJ@192.168.1.109>
2013-01-19 17:55:25 -0500

    last change

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
@@ -1,16 +1,48 @@
-package com.PhoebusHighSchool.PhoebusRobotics.UltimateAscent;
-
 /*
- * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phoebushighschool.phoebusrobotics.ultimateascent;
+
+import edu.wpi.first.wpilibj.AnalogModule;
+
+/**
+ *
+ * @author David
  */
 public class Ultrasonic {
 
-  
-  /** 
-   *  This method will find the distance to the closest thing in the cone of view.
-   */
-  public double findDistance() {
-  return 0.0;
-  }
+    AnalogModule ultrasonicSensor;
+    int channel;
 
+    /**
+     * Constructor that uses the default module slot
+     *
+     * @param analogChannel channel the ultrasonic sensor is connected to on the
+     * cRIO 9201 module.
+     */
+    public Ultrasonic(int analogChannel) {
+        channel = analogChannel;
+        ultrasonicSensor = AnalogModule.getInstance(1);
+    }
+
+    /**
+     * Return the actual distance to a target in inches.
+     *
+     * The distance is based on the current voltage values.
+     *
+     * @returns double - distance to the target in inches
+     */
+    public double getDistance() {
+        double value = ultrasonicSensor.getVoltage(channel);
+        return (value / (5.0 / 512.0));  //TODO: fix this value
+    }
+
+    public double pidGet() {
+        return getDistance();
+    }
+
+    public double getVoltage() {
+        return ultrasonicSensor.getVoltage(channel);
+    }
 }
