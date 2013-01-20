0f4833bd175f60e77b87f92c8474e259b00ec28d
djennings001 <djennings001@PHS-B22B-20.cte.hampton.k12.va.us>
2013-01-20 13:09:28 -0500

    last change

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/Ultrasonic.java
@@ -4,19 +4,27 @@ import edu.wpi.first.wpilibj.AnalogModule;
 /*
  * 
  */
+
 public class Ultrasonic {
-        AnalogModule ultrasonic;
-        protected int channel;
+
+    AnalogModule ultrasonic;
+    protected int channel;
+
     public Ultrasonic(int analogChannel) {
         channel = analogChannel;
         ultrasonic = AnalogModule.getInstance(1);
     }
-  /** 
-   *  This method will find the distance to the closest thing in the cone of view.
-   */
-  public double getDistance() {
-      double value = ultrasonic.getVoltage(channel);
-  return (value / 5.0) / 512.0;
-  }
 
+    /**
+     * This method will find the distance to the closest thing in the cone of
+     * view.
+     */
+    public double getDistance() {
+        double value = ultrasonic.getVoltage(channel);
+        return (value / 5.0) / 512.0;
+    }
+
+    public double getVoltage() {
+        return ultrasonic.getVoltage(channel);
+    }
 }
