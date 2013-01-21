8b10ac51bca321cac21bcb91c62b1c775b500f18
amontgomery001 <amontgomery001@PHS-B22B-21.cte.hampton.k12.va.us>
2013-01-21 16:54:29 -0500

    Merged master with Anna branch for TankDrive, Tread, Parameters and Arm class

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/TankDrive.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/TankDrive.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 /*
  */
-public class TankDrive implements PIDOutput {
+public class TankDrive implements PIDOutput 
+{
 
     protected UltimateAscentBot robot;
     protected ClimbingSystem leftArm;
     protected Tread rightTread;
     protected Tread leftTread;
+    double speed;
     
     //constructors
     public TankDrive() throws CANTimeoutException
@@ -21,39 +23,62 @@ public class TankDrive implements PIDOutput {
     }
 
   /** 
-   *  This method takes a joystick value, and turns the robot according to 
-   *  the value
+   *  This method takes a percent value, and turns the robot according to 
+   *  the value where positive values are to the right and negative values
+   *  are to the left.
    * 
+   * when robot is stationary: one tread moves forward while other tread moves 
+   *                           in reverse
+   * 
+   * when robot is moving: 
    * 
    * @param speedToTurn - number in the range of -1.0 .. 0.0 .. 1.0 where
    *                      0.0 is not turning and 1.0 is turning full speed 
    *                      {left/right} and -1.0 is turning full speed {right/
    *                      left}
    */
-  public void turn(double speedToTurn) 
+  public void turn(double speedToTurn) throws CANTimeoutException
   {
-      turn (speedToTurn);
+      if (speed == 0.0)
+      {
+          leftTread.drive(speedToTurn);
+          rightTread.drive(speedToTurn * -1.0);
+      }
+      else
+      {
+          
+      }
   }
 
   /** 
    *  This method will set the percent speed and direction of both treads.
    */
-  public void drive(double percent) 
+  public void drive(double percent) throws CANTimeoutException
   {
-      drive (percent); 
+      speed = percent;
+      leftTread.drive (percent); 
+      rightTread.drive(percent);
   }
 
   /**
-   * 
+   * This method takes a joystick value, and turns the robot according to 
+   *  the value
    * 
    * @param speedToTurn - number in the range of -1.0 .. 0.0 .. 1.0 where
    *                      0.0 is not turning and 1.0 is turning full speed 
    *                      {left/right} and -1.0 is turning full speed {right/
    *                      left}
    */
-  public void pidWrite(double speedToTurn) 
+  public void pidWrite(double speedToTurn)
   {
-    turn (speedToTurn);  
+      try
+      {
+        turn (speedToTurn);  
+      }
+      catch (CANTimeoutException e)
+      {
+          throw new RuntimeException(e.getMessage());
+      }
   }
 
 }
