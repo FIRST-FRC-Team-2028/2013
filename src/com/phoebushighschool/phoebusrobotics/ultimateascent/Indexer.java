073c2575b3f497f6a71b83e35622089ae89facc7
djennings7 <djennings4765@yahoo.com>
2013-02-03 15:01:32 -0500

    Update src/com/phoebushighschool/phoebusrobotics/ultimateascent/Indexer.java

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/Indexer.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/Indexer.java
@@ -1,4 +1,4 @@
-package com.PhoebusHighSchool.PhoebusRobotics.UltimateAscent;
+package com.phoebushighschool.phoebusrobotics.ultimateascent;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
