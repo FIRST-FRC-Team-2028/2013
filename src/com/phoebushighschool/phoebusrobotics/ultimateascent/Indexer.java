e87b3faa22bf8c4f024bc508044030b06df15476
ablank001 <ablank001@PHS-B22B-21.cte.hampton.k12.va.us>
2013-02-12 16:22:23 -0500

    Cleaned up Indexer code

--- src/com/phoebushighschool/phoebusrobotics/ultimateascent/Indexer.java
+++ src/com/phoebushighschool/phoebusrobotics/ultimateascent/Indexer.java
@@ -18,42 +18,21 @@ public class Indexer {
   {
       gameMech = new GameMech();
       indexer = new Solenoid(Parameters.DiscIndexerSolenoidChannel);
-      discPreIndex = new DigitalInput(Parameters.DiscInShooterGPIOChannel);
-      discCountCurrent = Parameters.discCountInit;
-      indexerHasRun = false;
-      indexerDiscPass = false;
+      
   }
-
   /** 
    *  This method will index one disc into the shooter.
    */
-  public void indexOneDisc() {
-      if (discPreIndex.get() && discCountCurrent >= 1 && !indexerHasRun && !indexerDiscPass){
-          indexer.set(true);           
-      }
-      else if (!discPreIndex.get() && !indexerHasRun){                
-          indexerDiscPass = true;
-      }
-      else if (discPreIndex.get() && indexerDiscPass){
-          indexer.set(false);
-          indexerDiscPass = false;
-          indexerHasRun = true;
-      }
-      else if (discCountCurrent >= 1 && indexerHasRun) {
-          discCountCurrent = discCountCurrent - 1;
-          indexerHasRun = false;
-      }
-      else {
-          indexer.set(false);
-      }
+      
+  public void setIndexerPiston(boolean value){
+      indexer.set(value);
   }
 
   public boolean isDiscLoaded() {  
     return discPreIndex.get();
   }
   
-  public int getDiscCountCurrent()
-  {
+  public int getDiscCountCurrent() {
       return discCountCurrent;
   }
 }
